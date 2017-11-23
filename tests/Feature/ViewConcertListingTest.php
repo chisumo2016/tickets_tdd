54eca8a75a72a201878f700db04579e7e46c5c6b
= <=>
2017-11-23 09:27:57 +0000

    initial commit

---
+++ tests/Feature/ViewConcertListingTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ViewConcertListingTest extends TestCase
+{
+    /**
+     * A basic test Concert.
+     *
+     * @tesr
+     */
+    public function user_can_view_a_concert_listing()
+    {
+        //Arrange   1
+           // Create a Concert
+
+
+
+        //Act       2
+        // View the concert Listing
+
+        // Assert   3
+        //
+        // See the concert details
+    }
+}
