badf977400ae7de03dbd85192a329e2ed7b64081
= <=>
2017-11-27 09:59:30 +0000

    Problem

---
+++ app/Concert.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Concert extends Model
+{
+    //
+    protected  $guarded = [];
+    protected  $dates = ['date'];
+}
--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -1,55 +1,22 @@
 <?php
 
-namespace Tests\Feature;
 
+use App\Concert;
 use Carbon\Carbon;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ViewConcertListingTest extends TestCase
 {
-    /**
-     * A basic test Concert.
-     *
-     * @tesr
-     */
-    public function user_can_view_a_concert_listing()
+    use DatabaseMigrations;
+    /** @test */
+    function user_can_view_a_published_concert_listing()
     {
-        //Arrange   1
-           // Create a Concert
-           $concert =  Concert ::create([
-
-               'title'                   => 'The Red Chord',
-               'subtitle'                => 'with Animosity and Lethargy',
-               'date'                    => Carbon::parse('December 23, 2017 8:00pm'),
-               'ticket_price'            =>3205,
-               'venue'                   =>'The Marsh Pit',
-               'venue_address'           =>'123 Example Lane',
-               'city'                    => 'Laraville',
-               'state'                   =>'ON',
-               'zip'                     =>'1716',
-               'additional_information'  =>'For tickets call (0555) 55555555'
-
-           ]);
-        //Act       2
-        // View the concert Listing
-
-           $this->visit('concerts/'.$concert->id);
-
-
-        // Assert   3
-        //
-        // See the concert details
-
-        $this->see('The Red Chord');
-        $this->see('with Animosity and Lethargy');
-        $this->see('December 23, 2017');
-        $this->see('8:00pm');
-        $this->see('32.05');
-        $this->see('The Marsh Pi');
-        $this->see('123 Example Lane');
-        $this->see('Laraville, ON 1716');
-        $this->see('For tickets call (0555) 55555555');
-        
+        gdgdgdggdgddgdhdhjdsdahdsahjdjashgdasdhjadhajdgdvcdasvhckjalcvasc.khasc
     }
+
+
 }
